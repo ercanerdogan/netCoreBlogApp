4a09e44b05e0c89ab193593327e0bf93bb7b59a1
Ercan-home <ercanerdogan@gmail.com>
2019-10-30 02:18:41 +0300

    setup http or https settings

--- Blog.WebUI/Startup.cs
+++ Blog.WebUI/Startup.cs
@@ -42,7 +42,7 @@ namespace Blog.WebUI
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
